ec6804ba56e8ee973351f4b83222f13834e2e48a
Active Volcano <chenqc@gmail.com>
2021-08-11 14:29:27 +0800

    Add simple alternative

---
+++ linux-status/simple-alternative.sh
@@ -0,0 +1,18 @@
+mkdir linux.status
+cd linux.status
+date --rfc-3339=seconds > date--rfc-3339.txt 2>&1
+df -h > df-h.txt 2>&1
+fdisk -l > fdisk-l.txt 2>&1
+mount > mount.txt 2>&1
+free -h > free-h.txt 2>&1
+ca /proc/meminfo > meminfo.txt 2>&1
+ca /proc/cpuinfo > cpuinfo.txt 2>&1
+ip addr > ip_addr.txt 2>&1
+netstat -anp > netstat-anp.txt 2>&1
+iptables -L -n > iptables-L-n.txt 2>&1
+firewall-cmd --list-port > firewall-cmd--list-port.txt 2>&1
+firewall-cmd --list-service > firewall-cmd--list-service.txt 2>&1
+route > route.txt 2>&1
+ps -auxww > ps-auxww.txt 2>&1
+docker ps > docker_ps.txt 2>&1
+set > set.txt 2>&1
